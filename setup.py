671b4975720801800d5b1a2b6f5ebecf07950e4a
longweiqiang <573925242@qq.com>
2026-01-12 19:11:56 +0800

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "get_holiday_cn",
-    version = "1.0.7",
+    version = "1.0.8",
     author = "Weiqiang.long",
     description = "获取中国法定节假日",
     long_description = long_description,
